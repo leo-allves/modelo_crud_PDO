0546da8e8c939fc233f61dc646a081c4b39b8c7c
leo-allves <leonardoalves1992@outlook.com>
2021-10-10 14:37:42 -0300

    Configurando BD

--- documentation/DAO.md
+++ documentation/DAO.md
@@ -5,65 +5,59 @@
 - Normalmente usamos classes para criar modelos
 - Depois criamos objetos
 
-### Olhando uma por um lado como classes teriamos uma tabelas atraves d euma classe
+### Olhando uma por um lado como classes teriamos uma tabelas atraves de uma classe
 
 CLASSE USUARIO
 
 ### PROPIEDADES
-
 - id
 - nome
 - email
 
 ### METODOS
+- getId
+- setId
 
-- getid
-- setid
+- getNome
+- setNome
 
-- getnome
-- setnome
-
-- getemail
-- setemail
+- getEmail
+- setEmail
 
 - tudo que fosse referente ao usuario passaria por essa classe
 
 1º Para implementar o DAO precisamos pelomenos de duas classes
 
-- deve se seguir não a risca mais o padrão principla DAO
+- deve se seguir não a risca mais o padrão principal DAO
 
 2º Para ter um DAO precisamos de 2 classes
 
-- Prieira sera o propio intem mesmo o usuario
+- Primeira sera o propio intem mesmo o usuario
 - segunda classe usuario DAO por exemplo
 
 ### CALSSE USUARIO
 
 PROPIEDADES
-
 - id
 - nome
 - email
 
 METODOS
+- getId
+- setId
 
-- getid
-- setid
+- getNome
+- setNome
 
-- getnome
-- setnome
-
-- getemail
-- setemail
+- getEmail
+- setEmail
 
 ### CLASSE USUARIO DAO
-
 - E aonde vai ter o nosso CRUD
 - Onde fazemos inserção, pegamos os dados, pegamos um dado só, delete, atualizar
 - Ele fará todo o processo em auxilio da classe usuario nesse exemplo
 
 ### TEREMOS NA CLASSE DAO
-
 METODOS
 
 - add(Usuario) <- receberia uma classe usuario
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-#PUXANDO CONFIG A CONEXÃO
+// Está página de listagem de produto
+//PUXANDO CONFIG A CONEXÃO
 require 'config.php';
-#PUXANDO O USUARIO DAO
+//PUXANDO O USUARIO DAO
 require './dao/UsuarioDaoMysql.php';
-#Instanciando classe UsuarioDaoMysql passando conexão $pdo
+//Instanciando classe UsuarioDaoMysql passando conexão $pdo
 $usuarioDao = new UsuarioDaoMysql($pdo);
-#pego todos os dados e jogo na var $lista
+//pego todos os dados e jogo na var $lista
 $lista = $usuarioDao->findAll();
 ?>
 <a href="adicionar.php">ADICIONAR USUÁRIO</a>
@@ -19,15 +20,15 @@ $lista = $usuarioDao->findAll();
     <!-- 
         pegando os objetos do array no meu findAll() e utilizando no meu formulario
     -->
-    <?php foreach($lista as $usuario): ?>
+    <?php foreach ($lista as $usuario): ?>
         <tr>
-            <td><?=$usuario->getId();?></td> 
-            <td><?=$usuario->getNome();?></td>
-            <td><?=$usuario->getEmail();?></td>
+            <td><?=$usuario->getId(); ?></td> 
+            <td><?=$usuario->getNome(); ?></td>
+            <td><?=$usuario->getEmail(); ?></td>
             <td>
                 <!-- Ações -->
-                <a href="editar.php?id=<?=$usuario->getId();?>">[ Editar ]</a>
-                <a href="excluir.php?id=<?=$usuario->getId();?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
+                <a href="editar.php?id=<?=$usuario->getId(); ?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?=$usuario->getId(); ?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
             </td>
         </tr>   
     <?php endforeach; ?>
